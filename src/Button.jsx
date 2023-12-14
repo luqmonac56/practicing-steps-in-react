74bb5fdc7cb569e76c21885ebdcc1fd4c0a41461
luqmonac56 <luqmonac56@gmail.com>
2023-12-14 05:08:03 +0100

    first commit

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Children } from "react";
+
+export default function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      {children}
+    </button>
+  );
+}
